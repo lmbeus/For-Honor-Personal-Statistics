bc10f94210bff6699c6270e7aaaa46fadfc72814
Loic Beus <lokioakenshield@outlook.com>
2020-06-27 01:23:44 -0600

    Revamped the menu to be cleaner and more readable and more efficient

--- For Honor Personal Statistics/For_Honor_Personal_Statistics.py
+++ For Honor Personal Statistics/For_Honor_Personal_Statistics.py
@@ -42,117 +42,144 @@ for i in hero_list:
             np.save(file, k)
             file.close
 
-
-#Begin menu/questionnaire
-print("Welcome to your personal For Honor statistics calculator!\n")
-print("Press 'q' at any time to quit the program")
-
 #Prompt for hero played/played against, win or loss, magnitude of win or loss and relative skill
-while (program_check != 'q'):
-    while (menu_check != 'q'):
-        hero = str(input("What hero did you play as? Please enter capitalized hero name\n"))
-        if hero == 'q':
-           program_check = 'q'
-           break
-        elif hero not in hero_list:
-            print("Invalid hero name, please try again\n\n")
-            continue        
-        
-        enemy = input("Who did you play against? Please enter capitalized hero name\n")
-        if enemy == 'q':
-           program_check = 'q'   
-           break
-        elif enemy not in hero_list:
-            print("Invalid hero name, please try again\n\n")
-            continue
+#FIXME: Add menu option 4
+while user_input != "q":
+    print("""
+    Welcome to your personal For Honor statistics calculator!
 
-        match_result = input("Did you win? Please enter 'yes' or 'no'\n")
-        if match_result == 'q':
-            program_check = 'q'
-            break
-        elif match_result == "yes":
-            print("What was the magnitude of your win? Enter one of the options below\n")
-            result_mag = input("'3-0' or '3-1' or '3-2'\n")
-            if (result_mag != "3-0") and (result_mag != "3-1") and (result_mag != "3-2"):
+    Main Menu:
+    Press 1 to see the list of heroes
+    Press 2 to enter new data
+    Press 3 to calculate statistics
+    Press m at any time to return to  main menu
+    Press q at any time to quit the program
+        """)
+    user_input = input()
+
+    if user_input == "1":
+        hero_list_it = iter(hero_list)
+        next(hero_list_it, None)
+        for i in hero_list_it:
+            print(i)
+        continue
+
+    elif user_input == "2":
+        while user_input != "q":
+            user_input = str(input("What hero did you play as? Please enter hero name\n"))
+            hero = user_input.capitalize()
+            if (user_input == "q") or (user_input == "m"):
+                break             
+            elif hero not in hero_list:
+                print("Invalid hero name, please try again\n\n")
+                continue        
+            
+            user_input = input("Who did you play against? Please enter hero name\n")
+            enemy = user_input.capitalize()
+            if (user_input == "q") or (user_input == "m"):
+                break
+            if enemy not in hero_list:
+                print("Invalid hero name, please try again\n\n")
+                continue
+
+            user_input = input("Did you win? Please enter 'yes' or 'no'\n")
+            match_result = user_input.lower()
+            if (user_input == "q") or (user_input == "m"):
+                break           
+            elif match_result == "yes" or match_result == "y":
+                print("What was the magnitude of your win? Enter one of the options below\n")
+                user_input = input("'3-0' or '3-1' or '3-2'\n")
+                result_mag = user_input
+                if (user_input == "q") or (user_input == "m"):
+                    break                
+                elif (result_mag != "3-0") and (result_mag != "3-1") and (result_mag != "3-2"):
+                    print("Invalid response, please try again\n\n")
+                    continue
+            elif match_result == "no" or match_result == "n":
+                print("What was the magnitude of your loss? Enter one of the options below\n")
+                user_input = input("'0-3' or '1-3' or '2-3'\n")
+                result_mag = user_input
+                if (user_input == "q") or (user_input == "m"):
+                    break
+                elif (result_mag != "0-3") and (result_mag != "1-3") and (result_mag != "2-3"):
+                    print("Invalid response, please try again\n\n")
+                    continue
+            else:
                 print("Invalid response, please try again\n\n")
                 continue
-        elif match_result == "no":
-            print("What was the magnitude of your loss? Enter one of the options below\n")
-            result_mag = input("'0-3' or '1-3' or '2-3'\n")
-            if (result_mag != "0-3") and (result_mag != "1-3") and (result_mag != "2-3"):
+
+            print("What was your skill leve realtive to your opponent? Enter one of the options below\n")
+            user_input = input("'higher' or 'same' or 'lower'\n")
+            skill_level = user_input.lower()
+            if (user_input == "q") or (user_input == "m"):
+                break
+            elif skill_level != "higher" and skill_level != "same" and skill_level != "lower":
                 print("Invalid response, please try again\n\n")
-                continue
-        else:
-            print("Invalid response, please try again\n\n")
-            continue
+                continue        
+            print("Thank you! The database will be updated\n\n")
 
-        print("What was your skill leve realtive to your opponent? Enter one of the options below\n")
-        skill_level = input("'higher' or 'same' or 'lower'\n")
-        if skill_level == 'q':
-            program_check = 'q'
-            break
-        elif skill_level != "higher" and skill_level != "same" and skill_level != "lower":
-            print("Invalid response, please try again\n\n")
-            continue        
-        break
-
-    if program_check == 'q':
-        break
-
-    print("Thank you! The database will be updated\n\n")
-
-    #Initialize variables and lists
-    new_data = []
-    overall_data = []
-    hero_data = []
-    infile = 'in???'
-    outfile = 'out???'
-    row_num = -69
+            #Initialize variables and lists
+            new_data = []
+            overall_data = []
+            hero_data = []
+            infile = 'in???'
+            outfile = 'out???'
+            row_num = -69
     
-    #Update the overall_data file with the new data from the questionnaire
-    infile = open("Overall_data", "rb")
-    new_data = np.load(infile, allow_pickle = True)
-    infile.close
-    outfile = open("Overall_data", "wb")
+            #Update the overall_data file with the new data from the questionnaire
+            infile = open("Overall_data", "rb")
+            new_data = np.load(infile, allow_pickle = True)
+            infile.close
+            outfile = open("Overall_data", "wb")
 
-    row_num = 0
-    overall_data = f.update_data(match_result, skill_level, result_mag, row_num, new_data)
-    np.save(outfile, overall_data)
-    outfile.close
+            row_num = 0
+            overall_data = f.update_data(match_result, skill_level, result_mag, row_num, new_data)
+            np.save(outfile, overall_data)
+            outfile.close
 
-    #Update hero specific data with the new data from the questionnaire
-    infile = open(hero + "_data", "rb") 
-    new_data = np.load(infile, allow_pickle = True)
-    infile.close
-    outfile = open(hero + "_data", "wb")
+            #Update hero specific data with the new data from the questionnaire
+            infile = open(hero + "_data", "rb") 
+            new_data = np.load(infile, allow_pickle = True)
+            infile.close
+            outfile = open(hero + "_data", "wb")
 
-    #Determine which row of the matrix to update based on the enemy fought
-    for i, j in enumerate (hero_list):
-        if enemy == j:
-            row_num = i - 1
+            #Determine which row of the matrix to update based on the enemy fought
+            for i, j in enumerate (hero_list):
+                if enemy == j:
+                    row_num = i - 1
+                    break
+
+            hero_data = f.update_data(match_result, skill_level, result_mag, row_num, new_data)
+            np.save(outfile, new_data)
+            outfile.close
+            
+            #Ask if they would like to quit or return to menu or calculate statistics
             break
-
-    hero_data = f.update_data(match_result, skill_level, result_mag, row_num, new_data)
-    np.save(outfile, new_data)
-    outfile.close
-
-    #Next menu options - calculate statistics or close program
-    while menu_check != "q":
-        user_input = input("Would you like to calculate statistics? Please enter 'yes' or 'no'\n\n")
-        if user_input == "no":
-            print("Goodbye :)")
-            program_check = "q"
-            break;
-        elif (user_input != "yes") and (user_input != "no"):
-            print("Invalid input, please try again")
-            continue
-        if user_input == "yes":
-            print("Which statistics would you like to calculate? Select an option below:\\n")
-            print("""
-            1. Overall data 
-            2. Hero data   
-            """)
-            user_input = input("Please enter '1' or '2'\\n")
+    
+        elif (user_input == "q") or (user_input == "m"):
+        continue
+    else:
+        print("Invalid Response")
+        continue
+    continue
+    ##Next menu options - calculate statistics or close program
+    #if user_input == 
+    #while menu_check != "q":
+    #    user_input = input("Would you like to calculate statistics? Please enter 'yes' or 'no'\n\n")
+    #    if user_input == "no":
+    #        print("Goodbye :)")
+    #        program_check = "q"
+    #        break;
+    #    elif (user_input != "yes") and (user_input != "no"):
+    #        print("Invalid input, please try again")
+    #        continue
+    #    if user_input == "yes":
+    #        print("Which statistics would you like to calculate? Select an option below:\\n")
+    #        print("""
+    #        1. Overall data 
+    #        2. Hero data   
+    #        """)
+    #        user_input = input("Please enter '1' or '2'\\n")
  
 
 
--- For Honor Personal Statistics/functions.py
+++ For Honor Personal Statistics/functions.py
@@ -4,7 +4,7 @@ import os
 
 def update_data(match_result, skill_level, result_mag, row_num, new_data):
     #Update the data based on the new data
-    if match_result == "yes":
+    if match_result == "yes" or match_result == "y":
         new_data[row_num, 0] += 1
         if result_mag == "3-0" and skill_level == "higher":
             new_data[row_num, 2] += 1 
@@ -24,7 +24,7 @@ def update_data(match_result, skill_level, result_mag, row_num, new_data):
             new_data[row_num, 9] += 1
         elif result_mag == "3-2" and skill_level == "lower":
             new_data[row_num, 10] += 1
-    elif match_result == "no":
+    elif match_result == "no" or match_result == "n":
         new_data[row_num, 1] += 1
         if result_mag == "0-3" and skill_level == "higher":
             new_data[row_num, 11] += 1 
